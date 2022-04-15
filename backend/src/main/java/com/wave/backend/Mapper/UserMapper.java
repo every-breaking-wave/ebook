e5226a609c4bf643bea844d7eae71cd825fedd31
every-breaking-wave <86960423+every-breaking-wave@users.noreply.github.com>
2022-04-16 02:56:48 +0800

    Delete backend directory

--- backend/src/main/java/com/wave/backend/Mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.wave.backend.Mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.wave.backend.model.domain.User;
-
-public interface UserMapper extends BaseMapper<User> {
-
-}
